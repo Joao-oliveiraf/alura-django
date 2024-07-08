cdca2846a0fb3dc03fa2ec2c5ff60b92d85bba09
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2024-07-08 14:35:22 -0300

    prep new model

--- galeria/views.py
+++ galeria/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
-
+    dados_da_imagem = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "webbtelescope.org / NASA / James Webb"},
+        2: {"nome": "Galáxia NGC 1079",
+            "legenda":"nasa.org / NASA / Hubble"}
+    }
+    return render(request, 'galeria/index.html', {"cards": dados_da_imagem})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
