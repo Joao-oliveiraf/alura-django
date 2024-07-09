d6c483434108fc385efbe8924462e66be2eea7eb
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2024-07-09 14:25:49 -0300

    Fotografia has published and date control features - working

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from galeria import models
 from galeria.models import Fotografia
 
+
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda", "categoria")
+    list_display = ("id", "nome", "legenda", "data_fotografia", "publicada")
     list_display_links = ("id", "nome")
+    list_editable = ("publicada",)
     search_fields = ("nome", "legenda")
     list_filter = ("categoria",)
     list_per_page = 10
