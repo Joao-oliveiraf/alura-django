43220ff2722449725da299d4d48a76cfba16c27c
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2024-07-09 15:48:13 -0300

    Fotografias can now be uploaded, templates img src altered

--- setup/urls.py
+++ setup/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from setup.settings import MEDIA_ROOT, MEDIA_URL
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
