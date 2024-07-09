6e4f35bc64659204dfd9c3e2427a7eaa6c64da21
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2024-07-09 19:45:18 -0300

    Search element implemented, needs more work

--- galeria/views.py
+++ galeria/views.py
@@ -9,3 +9,15 @@ def index(request):
 def imagem(request, foto_id):
     foto = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": foto})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+    if "test" in request.GET:
+        nome_a_buscar = request.GET['test']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+            return render(request, 'galeria/buscar.html', {"cards": fotografias})
+        elif nome_a_buscar == '':
+            return render(request, 'galeria/buscar.html')
+    
