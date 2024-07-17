885ad4a0f3fb3abfa59be8cf9d51da615ee4712e
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2024-07-17 18:21:32 -0300

    New feature to add images, changes to views, urls, forms, templates

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from apps.galeria.views import index, imagem, buscar
+from apps.galeria.views import index, imagem, buscar, nova_imagem,editar_imagem, deletar_imagem
 
 
 urlpatterns = [
     path('', index, name='home'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
-    path('buscar', buscar, name='buscar')
+    path('buscar', buscar, name='buscar'),
+    path('nova-imagem', nova_imagem, name='nova-imagem'),
+    path('editar-imagem', editar_imagem, name='editar-imagem'),
+    path('deletar-imagem', deletar_imagem, name='deletar-imagem')
 ]
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.db.models import Q
+from django.db import models
 from django.contrib import messages
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -33,4 +37,25 @@ def buscar(request):
             return render(request, 'galeria/buscar.html')
         else:
             return render(request, 'galeria/buscar.html')
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Faça login para adicionar novas imagens')
+        return redirect('login')
     
+    imagem = FotografiaForms
+    if request.method == 'POST':
+
+        imagem = FotografiaForms(request.POST) # HttpRequest.POST A dictionary-like object containing all given HTTP POST parameters
+
+        if imagem.is_valid():
+            imagem.save()
+            messages.success(request, 'Nova imagem adicionada!')
+            return redirect(request, 'home')
+
+    return render(request, 'galeria/nova-imagem.html', {'form': imagem})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
